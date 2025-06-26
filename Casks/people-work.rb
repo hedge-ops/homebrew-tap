f285a57ef395e10a3dcfa22dc51a58a6538e2f04
Hedge-Ops CI/CD <noreply@github.com>
2025-06-26 17:45:15 -0600

    fix sha

--- Casks/people-work.rb
+++ Casks/people-work.rb
@@ -1,6 +1,6 @@
 cask "people-work" do
   version "1.0.7"
-  sha256 "sha256:e92124e666e97f226c3585e1177b801ae9cf10e023184153deb29bc653618b27"
+  sha256 "e92124e666e97f226c3585e1177b801ae9cf10e023184153deb29bc653618b27"
   
   url "https://github.com/hedge-ops/app/releases/download/v1.0.7/People.Work.dmg"
   name "People Work"
