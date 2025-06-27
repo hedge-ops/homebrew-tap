7887cbe7d197d11cf816c57d2991f46f6bcd94de
Hedge-Ops CI/CD <noreply@github.com>
2025-06-27 09:31:48 -0600

    update sha

--- Casks/people-work.rb
+++ Casks/people-work.rb
@@ -1,6 +1,6 @@
 cask "people-work" do
   version "1.0.7"
-  sha256 "e92124e666e97f226c3585e1177b801ae9cf10e023184153deb29bc653618b27"
+  sha256 "bad87dcad76a35b1b5846d5337ad5bd6edb7de543586921ac40ccebf74489442"
   
   url "https://github.com/hedge-ops/people-work-releases/releases/download/v1.0.7/People.Work.dmg"
   name "People Work"
