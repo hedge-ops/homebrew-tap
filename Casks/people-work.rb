3b733f235c2aaed571afd7ba3ec0a620de372a68
Hedge-Ops CI/CD <noreply@github.com>
2025-06-27 09:29:19 -0600

    fix typo

--- Casks/people-work.rb
+++ Casks/people-work.rb
@@ -2,7 +2,7 @@ cask "people-work" do
   version "1.0.7"
   sha256 "e92124e666e97f226c3585e1177b801ae9cf10e023184153deb29bc653618b27"
   
-  url "https://github.com/hedge-ops/peopple-work-releases/releases/download/v1.0.7/People.Work.dmg"
+  url "https://github.com/hedge-ops/people-work-releases/releases/download/v1.0.7/People.Work.dmg"
   name "People Work"
   desc "Application for managing people data"
   homepage "https://people-work.io"
