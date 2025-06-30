27dd10082233a80b8b0d6c6c4e49a955c3cc99b2
anniehedgpeth <anniehedgpeth@users.noreply.github.com>
2025-06-30 17:27:17 +0000

    Update people-work to version 1.0.12

--- Casks/people-work.rb
+++ Casks/people-work.rb
@@ -1,8 +1,8 @@
 cask "people-work" do
-  version "1.0.10"
-  sha256 "f781ae5548271ce7d91ab515e59f651b8493a3acf5a7e7d5dc6eef9a959ddb43"
+  version "1.0.12"
+  sha256 "792d163c59e12e5bc58222b1fcb048106ac2e0f38132a81d853c373269741c10"
   
-  url "https://github.com/hedge-ops/people-work-releases/releases/download/v1.0.10/People.Work.dmg"
+  url "https://github.com/hedge-ops/people-work-releases/releases/download/v1.0.12/People.Work.dmg"
   name "People Work"
   desc "The operating system for the people-side of your job."
   homepage "https://people-work.io"
